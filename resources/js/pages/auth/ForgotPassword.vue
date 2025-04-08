a4f3129175af33cd503f51cf9c04c6d33eb9e2fa
Emma <emmalesbarreres@gmail.com>
2025-04-08 10:34:41 +0200

    traduction des pages

--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -22,8 +22,8 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthLayout title="Forgot password" description="Enter your email to receive a password reset link">
-        <Head title="Forgot password" />
+    <AuthLayout title="Mot de passe oublié" description="Entrez votre email pour recevoir un lien de réinitialisation de mot de passe">
+        <Head title="Mot de passe oublié" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
             {{ status }}
@@ -32,22 +32,22 @@ const submit = () => {
         <div class="space-y-6">
             <form @submit.prevent="submit">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="email@example.com" />
+                    <Label for="email">Adresse mail</Label>
+                    <Input id="email" type="email" name="email" autocomplete="off" v-model="form.email" autofocus placeholder="mail@example.com" />
                     <InputError :message="form.errors.email" />
                 </div>
 
                 <div class="my-6 flex items-center justify-start">
                     <Button class="w-full" :disabled="form.processing">
                         <LoaderCircle v-if="form.processing" class="h-4 w-4 animate-spin" />
-                        Email password reset link
+                        Lien de réinitialisation du mot de passe par e-mail
                     </Button>
                 </div>
             </form>
 
             <div class="space-x-1 text-center text-sm text-muted-foreground">
-                <span>Or, return to</span>
-                <TextLink :href="route('login')">log in</TextLink>
+                <span>Ou bien, revenez à</span>
+                <TextLink :href="route('login')">Se connecter</TextLink>
             </div>
         </div>
     </AuthLayout>
